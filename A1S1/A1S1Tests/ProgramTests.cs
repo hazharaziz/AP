ffa0ecf7e880f7fd86f9afd595fec81e80c5814e
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-03-08 16:15:00 +0330

    A1S1 Project added

---
+++ A1S1/A1S1Tests/ProgramTests.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A1S1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace A1S1.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void CalculateLengthTest()
+        {
+            string str = "This is a test string";
+            long expectedLength = 21;
+            long functionalLength = Program.CalculateLength(str);
+            Assert.AreEqual(expectedLength, functionalLength);
+        }
+
+        [TestMethod()]
+        public void LetterCountTest()
+        {
+            string str = "من همیشه سر وقت هستم";
+            long expectedChars = 16;
+            long functionalChars = Program.LetterCount(str);
+            Assert.AreEqual(expectedChars, functionalChars);
+        }
+
+        [TestMethod()]
+        public void LineCountTest()
+        {
+            string str = "hello how are you \n" +
+                "Is everything Ok \n" +
+                "Are you ok? \n" +
+                "This is great";
+            int expectedLines = 3;
+            int functionalLines = Program.LineCount(str);
+            Assert.AreEqual(expectedLines, functionalLines);
+
+
+        }
+
+
+
+        [TestMethod()]
+        public void FileLineCountTest()
+        {
+            string path1 = GetTestFile(out int expectedLines1,out int expectedChars1);
+            int functionalResult1 = Program.FileLineCount(path1);
+
+            string path2 = GetTestFile(out int expectedLines2, out int expectedChars2);
+            int functionalResult2 = Program.FileLineCount(path2);
+
+            Assert.AreEqual(expectedLines1, functionalResult1);
+            Assert.AreEqual(expectedLines2, functionalResult2);
+        }
+
+
+        [TestMethod()]
+        public void ListFilesTest()
+        {
+            
+            string[] expectedFiles1 = GetTestDir(out string dir1);
+            string[] functionalResult1 = Program.ListFiles(dir1);
+
+
+            string[] expectedFiles2 = GetTestDir(out string dir2);
+            string[] functionalResult2 = Program.ListFiles(dir2);
+            
+            CollectionAssert.AreEqual(expectedFiles1, functionalResult1);
+            CollectionAssert.AreEqual(expectedFiles2, functionalResult2);
+            
+
+        }
+
+        [TestMethod()]
+        public void FileSizeTest()
+        {
+            string path1 = GetTestFile(out int lineCount1,out int charCount1);
+            double expectedSize1 = charCount1;
+            double functionalSize1 = Program.FileSize(path1);
+
+            string path2 = GetTestFile(out int lineCount2,out int charCount2);
+            double expectedSize2 = charCount2;
+            double functionalSize2 = Program.FileSize(path2);
+
+            Assert.AreEqual(expectedSize1, functionalSize1);
+            Assert.AreEqual(expectedSize2, functionalSize2);
+
+        }
+
+
+
+        private static string GetTestFile(out int lineCount, out int charCount)
+        {
+            charCount = 0;
+            string tmpFile = Path.GetTempFileName();
+            lineCount = new Random(0).Next(10, 100);
+            List<string> lines = new List<string>();
+
+            for (int i = 0; i < lineCount; i++)
+            {
+                string line = $"Line number {i}";
+                charCount += line.Length + 2;
+                lines.Add(line);
+            }
+
+            File.WriteAllLines(tmpFile, lines);
+            return tmpFile;
+        }
+
+        private static string[] GetTestDir(out string tmpDir)
+        {
+            tmpDir = Path.GetTempFileName();
+            if (File.Exists(tmpDir))
+            {
+                File.Delete(tmpDir);
+            }
+
+            if (!Directory.Exists(tmpDir))
+            {
+                Directory.CreateDirectory(tmpDir);
+            }
+
+            else
+            {
+                foreach (string file in Directory.GetFiles(tmpDir))
+                {
+                    File.Delete(file);
+                }
+            }
+
+            int rndNum = new Random(0).Next(10, 20);
+            List<string> files = new List<string>();
+
+            for (int i = 0; i < rndNum; i++)
+            {
+                string filename = Path.Combine(tmpDir, $"file{i}.txt");
+                File.WriteAllText(filename, $"file{i}.txt content");
+                files.Add(filename);
+            }
+
+            return files.ToArray();
+        }
+        
+
+
+    }
+}
---
+++ A1S3/A1S3/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1S3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] files = Directory.GetFiles(@"..\..\TwitterData\Tweets");
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            files = Directory.GetFiles(@"..\..\TwitterData\Words");
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+            
+            
+            string filesPath = @"C:\git\AP97982\A1S3\A1S3\TwitterData\Tweets";
+            string[] textfiles = Directory.GetFiles(filesPath);
+
+            string resultTextPath = @"C:\git\AP97982\A1S3\A1S3\result.txt";
+            string posWordsPath = @"C:\git\AP97982\A1S3\A1S3\TwitterData\Words\positive.txt";
+            string negWordsPath = @"C:\git\AP97982\A1S3\A1S3\TwitterData\Words\negative.txt";
+
+            string filename;
+            string[] posWords = Q1_GetWords(posWordsPath);
+            string[] negWords = Q1_GetWords(negWordsPath);
+            double avgCharge = 0;
+            string[] lines; 
+            string content;
+            List<string> contents = new List<string>();
+
+
+            foreach (string file in textfiles)
+            {
+                filename = Path.GetFileName(file);
+
+                lines = File.ReadAllLines(file);
+                string[] tweets = new string[lines.Length - 1];
+                
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    tweets[i - 1] = lines[i];
+                }
+                
+
+                avgCharge = Q5_GetAvgPopChargeOfTweets(tweets, posWords, negWords);
+                content = $"{filename} : {avgCharge.ToString("0.000")}";
+                contents.Add(content);
+            }
+
+            File.WriteAllLines(resultTextPath, contents);
+            
+
+            
+            Console.ReadKey();
+        }
+
+        public static string[] Q1_GetWords(string path)
+        {
+            string[] words = File.ReadAllLines(path,Encoding.UTF8);
+
+            return words;
+
+        }
+
+        public static bool Q2_IsInWords(string[] words, string word)
+        {
+            if (words.Contains(word))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public static string[] Q3_GetWordsOfTweet(string tweet)
+        {
+            char[] tokens = new char[] { ' ', '\'', '\"', '@', '?', '!', ',','.','/',';','_',':','؟','،','(',')','/','-' };
+            string[] words = tweet.Split(tokens,StringSplitOptions.RemoveEmptyEntries);
+
+            return words;
+
+        }
+
+        public static int Q4_GetPopChargeOfTweet(string tweetString, string[] posWords, string[] negWords)
+        {
+            int posCharge = 0;
+            int negCharge = 0;
+
+            foreach (string posWord in posWords)
+            {
+                if (tweetString.Contains(posWord))
+                {
+                    posCharge += 1;
+                }
+                
+            }
+
+            foreach (string negWord in negWords)
+            {
+                if (tweetString.Contains(negWord))
+                {
+                    negCharge += -1;
+                }
+            }
+
+            int finalCharge = posCharge + negCharge;
+
+            return finalCharge;
+        }
+
+        public static double Q5_GetAvgPopChargeOfTweets(string[] tweets, string[] posWords, string[] negWords)
+        {
+            List<double> charges = new List<double>();
+
+            foreach (string tweet in tweets)
+            {
+                double tweetCharge = Q4_GetPopChargeOfTweet(tweet, posWords, negWords);
+                charges.Add(tweetCharge);
+            }
+
+            double AverageCharge = charges.Sum() / charges.Count;
+
+            return AverageCharge;
+        }
+
+
+
+
+    }
+}
