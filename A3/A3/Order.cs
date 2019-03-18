fbfa41630ebd1864b0b2a18527e0de6b2715eefc
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-03-18 23:09:12 +0330

    Order Class Implemented

--- A3/A3/Order.cs
+++ A3/A3/Order.cs
@@ -8,17 +8,40 @@ namespace A3
 {
     public class Order
     {
-        public List<Product> Products;
-        public bool IsDelivered;
+        public List<Product> products;
+        public bool isDelivered;
 
-        public Order(List<Product> products, bool isDelivered)
+        public Order(List<Product> productsList, bool isDeliveredProduct)
         {
+            products = productsList;
+            isDelivered = isDeliveredProduct;
+
         }
+
+        public List<Product> Products
+        {
+            get { return products;}
+            set { products = value; }
+        }
+
+        public bool IsDelivered
+        {
+            get { return isDelivered; }
+            set { isDelivered = value; }
+        }
+
+
 
         public float CalculateTotalPrice()
         {
-            //TODO
-            return -1;
+            float totalPrice = 0;
+
+            foreach (Product p in products)
+            {
+                totalPrice += p.Price;
+            }
+
+            return totalPrice;
         }
     }
 }
