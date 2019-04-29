2289134b123e3313f822caef6a99c4cd984599fd
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-04-29 12:47:28 +0430

    OddSum Method implemented

---
+++ E1B/E1B/BasicQuestions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1
+{
+    public class BasicQuestions
+    {
+        public static int OddSum(int[] nums)
+        {
+            int oddSum = 0;
+
+            foreach (int num in nums)
+            {
+                if (num % 2 != 0)
+                {
+                    oddSum += num;
+                }
+            }
+            return oddSum;
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+
+        }
+
+        public static void Swap(ref double a, ref double b)
+        { }
+
+        public static void Swap(ref long a, ref long b)
+        { }
+    }
+}
---
+++ E1B/E1B/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1B
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void m()
+        {
+
+        }
+    }
+}
