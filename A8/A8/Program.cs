5f626603187a21442ce94346b311acaa924d2e8e
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-05-07 02:30:15 +0430

    TestCases implemented

--- A8/A8/Program.cs
+++ A8/A8/Program.cs
@@ -10,20 +10,6 @@ namespace A8
     {
         static void Main(string[] args)
         {
-            DateTime c = new DateTime(2010, 3, 1);
-            DateTime d = new DateTime(2012, 2, 15);
-            DateTime e = new DateTime(2014, 2, 15);
-            DateTime f = new DateTime(2014, 2, 15);
-
-
-            Human h1 = new Human("sdm", "asjdnsnkd", c, 20);
-            Human h2 = new Human("dkndks", "dcnjdcn", d, 30);
-            Human h3 = new Human("slcmk", "sdks", e, 40);
-            Human h4 = new Human("slcmk", "sdks", e, 40);
-            //Console.WriteLine(h3 == h4);
-            //Console.WriteLine(h3.Equals(h4));
-
-            Console.WriteLine(h2.GetHashCode());
             Console.ReadKey();
         }
     }
