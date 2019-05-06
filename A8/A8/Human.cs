34e16647e1583a90ff317b058bc82fa4715ba976
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-05-06 21:43:13 +0430

    Human Class constructor implemented

---
+++ A8/A8/Human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace A8
+{
+    public class Human
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        DateTime BirthDate { get; set; }
+        double Height { get; set; }
+
+        /// <summary>
+        /// Human Class constructor
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="birthDate"></param>
+        /// <param name="height"></param>
+        public Human(string firstName, string lastName, DateTime birthDate, double height)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Height = height;
+        }
+    }
+}
