5801017185feeb8c912b6f64c0021f6bc4ec6428
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-05-12 19:04:09 +0430

    Project Files added

---
+++ A9/A9/ExceptionHandler.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A8
+{
+    public class ExceptionHandler
+    {
+        public string ErrorMsg { get; set; }
+        public readonly bool DoNotThrow;
+        private string _Input;
+
+        public string Input
+        {
+            get
+            {
+                try
+                {
+                }
+                catch
+                {
+                    if (!DoNotThrow)
+                        throw;
+                    ErrorMsg = "Caught exception in GetMethod";
+                }
+                return null;
+            }
+            set
+            {
+                try
+                {
+                }
+                catch
+                {
+                    if (!DoNotThrow)
+                        throw;
+                    ErrorMsg = "Caught exception in SetMethod";
+                }
+            }
+        }
+
+
+        public ExceptionHandler(
+            string input,
+            bool causeExceptionInConstructor,
+            bool doNotThrow=false)
+        {
+            DoNotThrow = doNotThrow;
+            this._Input = input;
+            try
+            {
+                if (causeExceptionInConstructor)
+                {
+                }
+            }
+            catch
+            {
+                if (!DoNotThrow)
+                    throw;
+                ErrorMsg = "Caught exception in constructor";
+            }
+        }
+
+        public void OverflowExceptionMethod()
+        {
+            try
+            {
+                checked
+                {
+                }
+            }
+            catch (OverflowException e)
+            {
+                if (!DoNotThrow)
+                    throw;
+                ErrorMsg = $"Caught exception {e.GetType()}";
+            }
+        }
+
+        public void FormatExceptionMethod()
+        {
+			try
+            {
+                int i = int.Parse(Input);
+            }
+            catch(FormatException e)
+            {
+                if (!DoNotThrow)
+                    throw;
+                ErrorMsg = $"Caught exception {e.GetType()}";
+            }
+        }
+
+        public void FileNotFoundExceptionMethod()
+        {
+ 
+        }
+
+        public void IndexOutOfRangeExceptionMethod()
+        {
+ 
+        }
+
+        public void OutOfMemoryExceptionMethod()
+        {
+            int[] a = new int[int.MaxValue];
+        }
+
+        public void MultiExceptionMethod()
+        {
+            try
+            {
+            }
+            catch (IndexOutOfRangeException e)
+            {
+            }
+            catch (OutOfMemoryException e)
+            {
+            }
+        }
+    }
+}
