f2a2f179319d78bb694fafc7e4a40c6ff4878563
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-05-23 12:20:41 +0430

    Account Class Constructor implemented

---
+++ A11/A11/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace A11
+{
+    public class Account
+    {
+        private double Balance { get; set; }
+
+        public Account(double balance)
+        {
+            if (balance >= 0)
+                Balance = balance;
+            else
+            {
+                Balance = 0;
+                Console.WriteLine($"Initial balance is invalid. Setting balance to 0.{Environment.NewLine}");
+            }
+        }
+
+        public void Credit(int amount)
+        {
+            try
+            {
+                if (amount < 0)
+                    throw new ArgumentException();
+                else
+                {
+                    Balance += amount;
+                }
+            }
+            catch(ArgumentException)
+            {
+                Console.WriteLine("Credit amount must be positive");
+            }
+        }
+
+    
+
+    }
+}
