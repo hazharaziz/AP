ca314a80f9a1be0354cef1277660454984b5d11d
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-06-18 01:59:41 +0430

    Sum Test Passed

--- A14/A14/ComputeState.cs
+++ A14/A14/ComputeState.cs
@@ -1,4 +1,6 @@
-﻿namespace A14
+﻿using System;
+
+namespace A14
 {
     /// <summary>
     /// ماشین حساب وقتی که جواب یک محاسبه
@@ -10,25 +12,39 @@
 
         public override IState EnterEqual()
         {
-            Calc.DisplayError("Syntax Error");
-            return new ErrorState(this.Calc);
+            if (Calc.Display.Contains("+"))
+            {
+                int[] numbers = Array.ConvertAll(Calc.Display.Split('+'), int.Parse);
+                int result = 0;
+                foreach (int i in numbers)
+                    result += i;
+                Calc.Display = $"{result}";
+                return new ComputeState(Calc);
+            }
+            return new ComputeState(Calc);
+
+            //Calc.DisplayError("Syntax Error");
+            //return new ErrorState(this.Calc);
         }
 
         public override IState EnterNonZeroDigit(char c)
         {
             // #3 لطفا!
-            return null;
+            this.Calc.Display += c;
+            return new ComputeState(this.Calc);
         }
 
         public override IState EnterZeroDigit()
         {
             // #4 لطفا
-            return null;
+            this.Calc.Display += "0";
+            return new ComputeState(this.Calc);
         }
 
         // #5 لطفا
         public override IState EnterOperator(char c)
         {
+            this.Calc.Display += c;
             return new ComputeState(this.Calc);
         }
 
