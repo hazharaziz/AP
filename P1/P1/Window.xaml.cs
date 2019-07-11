05f8e5f68fd7f2c658a99132d9da59acc383ec8c
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-07-11 17:40:09 +0430

    a1

---
+++ P1/P1/Window.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+
+            switch (index)
+            {
+                case 0:
+                    btn1.Background = Brushes.Aquamarine;
+                    break;
+                //case 1:
+                //    btn2.Background = Brushes.Aquamarine;
+                //    break;
+                case 2:
+                    Window.Close();
+                    break;
+
+            }
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            int index = int.Parse(((Button)e.Source).Uid);
+
+            GridCursor.Margin = new Thickness(10 + (100 * index), 35, 10, 0);
+
+            switch (index)
+            {
+                case 0:
+                    GridMain.Background = Brushes.Aquamarine;
+                    break;
+                case 1:
+                    GridMain.Background = Brushes.LightBlue;
+                    break;
+                case 2:
+                    GridMain.Background = Brushes.LawnGreen;
+                    break;
+            }
+
+
+        }
+
+    }
+}
