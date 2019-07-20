c148dd67bf82f36c37f95c663dcb87fa4c511781
Hazhar Aziz <hazharaziz5555@gmail.com>
2019-07-21 00:03:14 +0430

    a12

--- P1/P1Tests/LinearEquationsTests.cs
+++ P1/P1Tests/LinearEquationsTests.cs
@@ -26,20 +26,11 @@ namespace P1Tests
             new LinearEquations("2x+3y+3z=0, 3x+5y+z=8,4y+3z=1")
         };
 
-
         [TestMethod()]
         public void SolutionStringTest()
         {
-            
-            Assert.AreEqual("x = -0.5", linearEquations[0].SolutionString);
-            //Assert.AreEqual(SolutionState.Unique, linearEquations.SolutionState);
-            //Assert.AreEqual(new Vector<double>(2) { 1, 1 }, linearEquations.RightSideVector);
-
-            
+            Assert.AreEqual("x = -0.5", linearEquations[0].SolutionString);            
             Assert.AreEqual("No Solution", linearEquations[1].SolutionString);
-            //Assert.AreEqual(SolutionState.Unique, linearEquations.SolutionState);
-            //CollectionAssert.AreEqual(new List<string>() { "2x+3y ", " 3x+ 2y " }, linearEquations.LeftSideEquations);
-
             Assert.AreEqual("No Solution", linearEquations[2].SolutionString);
             Assert.AreEqual("x = -1.0 , y = 2.0 , z = 0.0 , t = 1.0", linearEquations[3].SolutionString);
             Assert.AreEqual("x = 2.0 , y = -1.3 , z = 1.8", linearEquations[4].SolutionString);
@@ -47,24 +38,63 @@ namespace P1Tests
             Assert.AreEqual("No Unique Solution", linearEquations[6].SolutionString);
             Assert.AreEqual("No Solution", linearEquations[7].SolutionString);
             Assert.AreEqual("x = 0.4 , y = 1.8 , z = -2.0", linearEquations[8].SolutionString);
+        }
+
+        [TestMethod()]
+        public void SolutionStateTest()
+        {
+            Assert.AreEqual(SolutionState.Unique, linearEquations[0].SolutionState);
+            Assert.AreEqual(SolutionState.NoSolution, linearEquations[1].SolutionState);
+            Assert.AreEqual(SolutionState.NoSolution, linearEquations[2].SolutionState);
+            Assert.AreEqual(SolutionState.Unique, linearEquations[3].SolutionState);
+            Assert.AreEqual(SolutionState.Unique, linearEquations[4].SolutionState);
+            Assert.AreEqual(SolutionState.Infinite, linearEquations[5].SolutionState);
+            Assert.AreEqual(SolutionState.Infinite, linearEquations[6].SolutionState);
+            Assert.AreEqual(SolutionState.NoSolution, linearEquations[7].SolutionState);
+            Assert.AreEqual(SolutionState.Unique, linearEquations[8].SolutionState);
+        }
+
+        [TestMethod()]
+        public void LeftSideEquationsTest()
+        {
+            CollectionAssert.AreEqual(new List<string>() { "+2x" }, linearEquations[0].LeftSideEquations);
+            CollectionAssert.AreEqual(new List<string>() { "+2x+y","+2x+y" }, linearEquations[1].LeftSideEquations);
+            CollectionAssert.AreEqual(new List<string>() { "+2x+y+z","+x+z","+x+y" }, linearEquations[2].LeftSideEquations);
+            CollectionAssert.AreEqual(new List<string>() { "+2x+y+z+t", "+x+y+t","+y+t+z","+x+y+3t" }, linearEquations[3].LeftSideEquations);
+            CollectionAssert.AreEqual(new List<string>() { "+2x+y-z","+2y+2z","+x+y-z" }, linearEquations[4].LeftSideEquations);
+            CollectionAssert.AreEqual(new List<string>() { "+x+3y+2z","+2x+y+4z","+x+2y+2z" }, linearEquations[5].LeftSideEquations);
+            CollectionAssert.AreEqual(new List<string>() { "+x+2y-z", "+2x-y+2z", "+3x-4y+5z" }, linearEquations[6].LeftSideEquations);
+            CollectionAssert.AreEqual(new List<string>() { "+2x-3y+4z","+12x+y-2z","+5x+2y-3z" }, linearEquations[7].LeftSideEquations);
+            CollectionAssert.AreEqual(new List<string>() { "+2x+3y+3z", "+3x+5y+z","+4y+3z" }, linearEquations[8].LeftSideEquations);
+        }
 
 
+        [TestMethod()]
+        public void RightSideVectorTest()
+        {
+            Assert.AreEqual(new Vector<double>(1) { -1 }, linearEquations[0].RightSideVector);
+            Assert.AreEqual(new Vector<double>(2) { 0,1 }, linearEquations[1].RightSideVector);
+            Assert.AreEqual(new Vector<double>(3) { 0,1,2 }, linearEquations[2].RightSideVector);
+            Assert.AreEqual(new Vector<double>(4) { 1,2,3,4 }, linearEquations[3].RightSideVector);
+            Assert.AreEqual(new Vector<double>(3) { 1,1,-1 }, linearEquations[4].RightSideVector);
+            Assert.AreEqual(new Vector<double>(3) { 1,2,1 }, linearEquations[5].RightSideVector);
+            Assert.AreEqual(new Vector<double>(3) { 4,3,2 }, linearEquations[6].RightSideVector);
+            Assert.AreEqual(new Vector<double>(3) { 2,5,2 }, linearEquations[7].RightSideVector);
+            Assert.AreEqual(new Vector<double>(3) { 0,8,1 }, linearEquations[8].RightSideVector);
+        }
 
-
-            //Assert.AreEqual("x = 0.4 , y = 1.8 , z = -2.0", linearEquations.SolutionString);
-            //Assert.AreEqual(SolutionState.Unique, linearEquations.SolutionState);
-            //CollectionAssert.AreEqual(new List<double>() { 2, 3, 0 }, linearEquations.VarCoefficients['x']);
-
-            //Assert.AreEqual("No Solution", linearEquations.SolutionString);
-            //Assert.AreEqual(SolutionState.NoSolution, linearEquations.SolutionState);
-            //Assert.IsTrue(new Matrix<double>(3, 3) {
-            //                new Vector<double>(3){2,-3,4 },
-            //                new Vector<double>(3){12,1,-2 },
-            //                new Vector<double>(3){5,2,-3 } } == linearEquations.CoefficientMatrix);
-
-            //Assert.AreEqual("No Unique Solution", linearEquations.SolutionString);
-            //Assert.AreEqual(SolutionState.Infinite, linearEquations.SolutionState);
-            //Assert.IsTrue(new Vector<double>(3) { 3, 4, 2 } != linearEquations.RightSideVector);
+        [TestMethod()]
+        public void VariableCoefficientsTest()
+        {
+            CollectionAssert.AreEqual(new List<double>() { 2 }, linearEquations[0].VarCoefficients['x']);
+            CollectionAssert.AreEqual(new List<double>() { 1,1 }, linearEquations[1].VarCoefficients['y']);
+            CollectionAssert.AreEqual(new List<double>() { 1,1,0 }, linearEquations[2].VarCoefficients['z']);
+            CollectionAssert.AreEqual(new List<double>() { 1,1,1,3 }, linearEquations[3].VarCoefficients['t']);
+            CollectionAssert.AreEqual(new List<double>() { 2,0,1 }, linearEquations[4].VarCoefficients['x']);
+            CollectionAssert.AreEqual(new List<double>() { 3,1,2 }, linearEquations[5].VarCoefficients['y']);
+            CollectionAssert.AreEqual(new List<double>() { -1,2,5 }, linearEquations[6].VarCoefficients['z']);
+            CollectionAssert.AreEqual(new List<double>() { 2,12,5 }, linearEquations[7].VarCoefficients['x']);
+            CollectionAssert.AreEqual(new List<double>() { 3,5,4 }, linearEquations[8].VarCoefficients['y']);
         }
     }
 
@@ -81,12 +111,32 @@ namespace P1Tests
 
 
         [TestMethod()]
-        public void NormalEquationTests()
+        public void SplitEquationTests()
         {
-            //equation = new DiagramEquation("x^2 + 2 + x", EquationType.Normal);
-            //CollectionAssert.AreEqual(new string[] { "x^2", "+2", "+x" }, equation.Split(equation.EquationString));
-            //Assert.AreEqual(4, equation.ParseNormalEquation(1, equation.EquationString));
-            //Assert.AreEqual(120, equation.Factorial(5));
+            CollectionAssert.AreEqual(new string[] { "2x", "+x^2", "+x" }, diagramEquations[0].Split(diagramEquations[0].EquationString));
+            CollectionAssert.AreEqual(new string[] { "x", "+1" }, diagramEquations[1].Split(diagramEquations[1].EquationString));
+            CollectionAssert.AreEqual(new string[] { "x^3", "+x^4", "+x^5","+2x","+1" }, diagramEquations[2].Split(diagramEquations[2].EquationString));
+            CollectionAssert.AreEqual(new string[] { "3x", "+4x^2", "+5x^3" }, diagramEquations[3].Split(diagramEquations[3].EquationString));
+        }
+
+        [TestMethod()]
+        public void ParseEquationTest()
+        {
+            Assert.AreEqual(10, diagramEquations[0].ParseNormalEquation(2, diagramEquations[0].EquationString));
+            Assert.AreEqual(18, diagramEquations[0].ParseNormalEquation(3, diagramEquations[0].EquationString));
+            Assert.AreEqual(0, diagramEquations[0].ParseNormalEquation(0, diagramEquations[0].EquationString));
+
+            Assert.AreEqual(11, diagramEquations[1].ParseNormalEquation(10, diagramEquations[1].EquationString));
+            Assert.AreEqual(221, diagramEquations[1].ParseNormalEquation(220, diagramEquations[1].EquationString));
+            Assert.AreEqual(41, diagramEquations[1].ParseNormalEquation(40, diagramEquations[1].EquationString));
+
+            Assert.AreEqual(6, diagramEquations[2].ParseNormalEquation(1, diagramEquations[2].EquationString));
+            Assert.AreEqual(-2, diagramEquations[2].ParseNormalEquation(-1, diagramEquations[2].EquationString));
+            Assert.AreEqual(61, diagramEquations[2].ParseNormalEquation(2, diagramEquations[2].EquationString));
+
+            Assert.AreEqual(5430, diagramEquations[3].ParseNormalEquation(10, diagramEquations[3].EquationString));
+            Assert.AreEqual(62, diagramEquations[3].ParseNormalEquation(2, diagramEquations[3].EquationString));
+            Assert.AreEqual(12, diagramEquations[3].ParseNormalEquation(1, diagramEquations[3].EquationString));
         }
     }
 
